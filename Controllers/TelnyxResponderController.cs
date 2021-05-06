997f4493f9dd8b65b9814813a0f691642e08a0ca
Ryan Overton <ryan.overton@outlook.com>
2021-05-06 18:13:53 -0500

    project creation

---
+++ Controllers/TelnyxResponderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace telnyx_responder.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TelnyxResponderController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,6 @@
+# How to respond to an SMS message using Telnyx and C#
+
+## Pre-requisites
+- Telnyx account
+- [.NET 5](https://dotnet.microsoft.com/download/dotnet/5.0)
+- [NGROK](https://ngrok.com/download)
